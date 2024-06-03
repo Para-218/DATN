096d9b2f5a43b2251a7d74fcd1484232b1938355
BuiThanhBinh <binh.s078251@gmail.com>
2024-06-04 00:17:02 +0700

    Adjust css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
 import './index.scss'
+import { useLocation } from 'react-router-dom'
 
 export const Header = () => {
-  return <div className='header'></div>
+  const location = useLocation()
+  const pathName = location.pathname
+  const navigate = pathName.split('/')[1]
+
+  return (
+    <div className='header'>
+      <h1>{navigate}</h1>
+    </div>
+  )
 }
